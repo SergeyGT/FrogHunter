5353b6f19c1e76696a9756c8f55980ec3c948ce9
SergeyGT <79414642+SergeyGT@users.noreply.github.com>
2025-03-04 19:00:38 +0300

    Update Scene

--- Assets/__Scripts/Player/Player.cs
+++ Assets/__Scripts/Player/Player.cs
@@ -67,7 +67,7 @@ public class Player : MonoBehaviour
     {
         _animator.SetBool("Hit", obj);
         Picked();
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(2.1f);
         Death?.Invoke();
         Destroy(this.gameObject);
     }
